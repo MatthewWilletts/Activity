113688250b490995a66f65f0d656eeda38159afd
Matthew Willetts <matthewwilletts@users.noreply.github.com>
2016-04-15 11:40:44 +0100

    Merge branch 'master' of https://github.com/MatthewWilletts/Activity First Commit

--- FirstLookAtData.R
+++ FirstLookAtData.R
@@ -1,3 +1,22 @@
 library(data.table)
+library(wavelets)
+library(HMM)
 
-data<-fread(input='/Users/Matthew/Documents/Oxford/Holmes/Prototype_data/test1.csv')
+
+#data<-fread(input='/Users/Matthew/Documents/Oxford/Holmes/Prototype_data/test1.csv')
+data<-read.csv(file='/Users/Matthew/Documents/Oxford/Holmes/Prototype_data/p064ActivityEpoch_raw.csv',header=FALSE)
+data$V6<-NULL
+
+data<-data[c( rep(FALSE,4), TRUE ),]
+
+small_data_wvlt<-dwt(data$V5[1:2^15])
+data_wvlt<-dwt(data$V5)
+test<-Matrix(0,742,7)
+for(i in 1:7)
+{
+  test[,i]=rep(unlist(data_wvlt@W[i]),each=2^(i-1))[1:742]
+  
+}
+test<-unlist(data_wvlt@W)
+
+baumWelch(HMM, observations, maxIterations=1000, delta=1E-9, pseudoCount=0)
