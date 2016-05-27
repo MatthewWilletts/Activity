b319ccaf100dd989b4404199ac708d359ece3867
Matthew Willetts <matthewwilletts@users.noreply.github.com>
2016-05-27 13:49:23 +0100

    This one might actually work

--- clusterAnalysis.R
+++ clusterAnalysis.R
@@ -7,9 +7,22 @@ library(RSpectra)
 library(mhsmm)
 library(parallel)
 library(stringr)
-
+library(purrr)
+library(doMC)
+library(foreach)
+library(MASS)
 
 source('/home/dph-ukbaccworkgroup/magd4534/Activity/clusterFunctions.R')
+
+
+ncores<-16
+
+ncores<-2
+
+
+set.seed(100)
+
+registerDoMC(ncores)
 
 #First, define data directories
 
@@ -17,16 +30,18 @@ dataDirectory<-'/data/dph-ukbaccworkgroup/npeu0203/capture-processed'
 labelDirectory<-'/data/dph-ukbaccworkgroup/npeu0203/label-data/label-dictionary-9-classes'
 instanceLabelDirectory<-'/data/dph-ukbaccworkgroup/magd4534/label-data/instance-label-dictionary-9-classes'
 outputDataDirectory<-'/data/dph-ukbaccworkgroup/magd4534/capture-processed'
-
+resultsDataDirectory<-'/data/dph-ukbaccworkgroup/magd4534/results'
+  
 dataDirectory<-'/Users/Matthew/Documents/Oxford/Activity/FeatureData'
 labelDirectory<-'/Users/Matthew/Documents/Oxford/Activity/LabelData'
 instanceLabelDirectory<-'/Users/Matthew/Documents/Oxford/Activity/InstanceLabelData'
-outputDataDirectory<-'/data/dph-ukbaccworkgroup/magd4534/capture-processed'
+outputDataDirectory<-dataDirectory
+resultsDataDirectory<-outputDataDirectory
 
 
 #Directories for RF and HMM models
-RFoutput<-paste0(dataDirectory,'/RFoutput')
-HMMoutput<-paste0(dataDirectory,'/HMMoutput')
+RFoutput<-paste0(resultsDataDirectory,'/RFoutput')
+HMMoutput<-paste0(resultsDataDirectory,'/HMMoutput')
 
 #Directories for predictions
 Predictions<-paste0(dataDirectory,'/Predictions')
@@ -66,95 +81,236 @@ FeatureData<-list()
 #Now load up Instance data and Feature Data
 dropvals<-c(4)
 
-Data<-lapply(X = jointFiles,FUN = function(x) cleanData(jointFiles = x,
+Data<-lapply(X = jointFiles,FUN = function(x) try(cleanData(jointFiles = x,
                                                         drop=dropvals,
                                                         instanceLabelDirectory = instanceLabelDirectory,
                                                         labelDirectory = labelDirectory,
                                                         dataDirectory = dataDirectory,
-                                                        outputDataDirectory=outputDataDirectory))
-                                                        #,mc.cores = 10)
+                                                        outputDataDirectory=outputDataDirectory)))
+                                                        #,mc.cores = ncores)
+
+#remove errored participants
+deleteParticipants<-(which(sapply(Data,length)==1))
+Data[deleteParticipants]<-NULL
+
+
+LDAperformance<-list()
+HMMperformance<-list()
+
+
+#AllFeatureData<-rbindlist(Data %>% map(c("labelledData", "labelledFeatureData")))
+#AllFFTData<-rbindlist(Data %>% map(c("labelledData", "labelledFFTData")))
+
+AllFeatureData<-rbindlist(lapply(X = lapply(X=Data,'[[',"labelledData"), '[[',"labelledFeatureData"))
+AllFeatureData$dataErrors<-NULL
+AllFeatureData$clipsBeforeCalibr<-NULL
+AllFeatureData$clipsAfterCalibr<-NULL
+AllFeatureData$rawSamples<-NULL
+AllFeatureData$samples<-NULL
+
+
+AllFFTData<-rbindlist(lapply(X = lapply(X=Data,'[[',"labelledData"), '[[',"labelledFFTData"))
+
+#ncolFmatrix<-sum(ncol(AllFeatureData[,2:12,with=FALSE]),ncol(AllFFTData[,2:252,with=FALSE]))
+AllFeatureData<-(cbind(AllFeatureData[,2:12,with=FALSE],AllFFTData[,2:251,with=FALSE]))
+
+#ncol = ncolFmatrix, byrow = TRUE)
+#AllFeatureData2<-mmult1(as.matrix(AllFeatureData[,2:12,with=FALSE]),as.matrix(AllFFTData[,2:252,with=FALSE]))
+
+#AllInstanceData<-rbindlist(Data %>% map(c("labelledData", "instanceLabelData")))
+
+AllInstanceData<-rbindlist(lapply(X = lapply(X=Data,'[[',"labelledData"), '[[',"instanceLabelData"))
+behaviorAsFactor<-as.factor(AllInstanceData$behavior)
+
+
+#ReduceInstanceData<-reduceLabels(data=AllInstanceData,labelsToReduce=list(c('gardening','standing'),c('in-vehicle')),overallLabel =c('sitting','driving'))
+
+
+#For now, only work with labelled data
+ix<-which(!AllInstanceData$behavior=='unknown')
+
+
+#Create testing data - leave one out
+participants<-lapply(X = jointInstanceFiles[-deleteParticipants], function (x) gsub(pattern = '.csv',replacement = '',x = x))
+behaviors<-unique(AllInstanceData$behavior)
+
+for(participant in participants){
+  
+  
+iq<-which(AllInstanceData$identifier[ix]==participant)
+
+  
+#1.a Run RF using the labelled data points for all but one participant's data
+
+ntree=1000
+mtry = floor(sqrt(ncol(AllFeatureData[ix,])))
+replace=TRUE
+nodesize=1
+
+
+rf <- foreach(ntree=rep(floor(100/ncores),ncores), .combine=randomForest::combine, .packages='randomForest') %dopar%
+  randomForest(x = AllFeatureData[ix[-iq],],y=behaviorAsFactor[ix[-iq]],
+                 ntree=ntree,
+                 mtry=mtry,
+                 replace=replace,
+                 nodesize=nodesize,
+                 importance=TRUE,
+                 proximity = FALSE,
+                 do.trace = TRUE)
+
+
+#1.b we need to know which data point goes to which node of each tree in our training set!
+training_nodes <- foreach(features=split(AllFeatureData[ix[-iq],],1:ncores),.combine = rbind, .packages='randomForest') %dopar% 
+          attr(predict(rf, features, type="prob",
+                  norm.votes=TRUE, predict.all=TRUE, proximity=FALSE, nodes=TRUE,oob.prox=TRUE),which='nodes')
+
+training_nodes<-training_nodes[order(as.numeric(row.names(training_nodes))),]
+
+#Challenge - create proximity matrix from node locations
+
+#training_nodes matrix has npoints rows and ntree columns
+
+#Proximity matrix is npoints by npoints
+
+ProxTrain<-computeProximity(nodes1=training_nodes,nodes2=training_nodes,parallel = TRUE,mc.cores = ncores)
+
+
+#2.Output the RF proximity matrix, for all testing data training data - ie for one participant
+
+testing_nodes <- foreach(features=split(AllFeatureData[ix[iq],],1:ncores),.combine = rbind, .packages='randomForest') %dopar% 
+        attr(predict(rf, features, type="prob",
+               norm.votes=TRUE, predict.all=TRUE, proximity=FALSE, nodes=TRUE),which='nodes')
+
+reordering_indices<-order(as.numeric(row.names(testing_nodes)))
+testing_nodes<-testing_nodes[reordering_indices,]
+
+
+#and also the RF predicitions
+testing_RF_predicitions<-foreach(features=split(AllFeatureData[ix[iq],],1:ncores),.combine = c, .packages='randomForest') %dopar% 
+        as.character(predict(rf, features, type="response",
+               norm.votes=TRUE, predict.all=TRUE, proximity=FALSE, nodes=FALSE)$aggregate,names)
+
+
+testing_RF_predicitions<-(testing_RF_predicitions[reordering_indices])
+
+
+#Challenge - create proximity matrix from node locations of overlap between Training data and Testing Data
+
+#testing_nodes matrix has ntraingpoints rows and ntree columns
+
+#Proximity matrix is ntestingpoints rows by ntrainingpoints columns
+
+
+ProxTest<-computeProximity(nodes1=testing_nodes,nodes2=training_nodes,parallel = TRUE,mc.cores = ncores)
 
 
 
-performance<-list()
+#3.Using ideas from spectral clustering, take an eigen(like) spectral decomposition of ProxTrain and project all
+# testing data points into the leading k-components of the decomposition of ProxTrain
+# with k smallish (say 3 or 4 dimensions)
 
-trainingNoLabel<-list()
-testingNoLabel<-list()
-# 
-# #Load up Data
-# for (i in 1:length(listOfIndividuals)){
-#   #create instance level information
-#   boutFileAddress<-paste(cleanBoutDirectory,listOfIndividuals[i],sep='/')
-#   if(file.exists(file.path(outputLabelDirectory,identifiers[i]))==FALSE){
-#     extractLabelsSingleFile(inputFile = boutFileAddress,outputDir = outputLabelDirectory,winSize = ws)
-#   }
-#   #create features
-#   accelFileAddress<-paste(cleanDataDirectory,listOfDataFiles[i],sep='/')
-#   if(file.exists(file.path(outputFeatureDirectory,identifiers[i]))==FALSE){
-#     extractAccFeatsFile(inputFile = accelFileAddress,outputPath = file.path(outputFeatureDirectory,identifiers[i]),winSize = 60)
-#   }
-#   
-#   
-#   #Step 1 - Load up all data
-#   
-#   #Load in instance level labels
-#   InstanceData[[i]]<-NULL
-#   InstanceDir<-file.path(outputLabelDirectory,identifiers[i])
-#   InstanceFiles<-list.files(InstanceDir)
-#   tempInstanceData<-NULL
-#   
-#   #Load in features
-#   FeatureData[[i]]<-NULL
-#   tempFeatureData<-NULL
-#   FeatureDir<-file.path(outputFeatureDirectory,identifiers[i])
-#   FeatureFiles<-list.files(FeatureDir)
-#   
-#   
-#   
-#   for(k in 1:length(InstanceFiles)){
-#     
-#     #load in instance data
-#     temptempInstanceData<-read.csv(file=file.path(InstanceDir,InstanceFiles[k]),stringsAsFactors = F)
-#     
-#     #load in feature data
-#     temptempFeatureData<-read.csv(file=file.path(FeatureDir,FeatureFiles[k]),stringsAsFactors = F)
-#     
-#     #discard all data before first labelled data point and after last labelled point
-#     kx<-which(!temptempInstanceData$behavior=='nolabel')
-#     if(length(kx)>0){
-#       maxIndex<-min(kx[length(kx)],nrow(temptempFeatureData))
-#       temptempInstanceData<-temptempInstanceData[kx[1]:maxIndex,]
-#       tempInstanceData<-rbind(tempInstanceData,temptempInstanceData)
-#       
-#       temptempFeatureData<-temptempFeatureData[kx[1]:maxIndex,]
-#       tempFeatureData<-rbind(tempFeatureData,temptempFeatureData)
-#     }
-#   }
-#   
-#   InstanceData[[i]]<-tempInstanceData
-#   rm(tempInstanceData)
-#   rm(temptempInstanceData)
-#   FeatureData[[i]]<-tempFeatureData
-#   rm(tempFeatureData)
-#   rm(temptempFeatureData)
-#   
-#   
-#   #cut down Instance data to size of Feature data, or vice versa
-#   
-#   if(nrow(FeatureData[[i]])<nrow(InstanceData[[i]])){
-#     InstanceData[[i]]<-InstanceData[[i]][seq(1,nrow(FeatureData[[i]])),]
-#   } else {
-#     FeatureData[[i]]<-FeatureData[[i]][seq(1,nrow(InstanceData[[i]])),]
-#   }
-#   
-#   
-#   #add participant labels to data
-#   InstanceData[[i]]$name<-identifiers[i]
-#   FeatureData[[i]]$name<-identifiers[i]
-#   
-#   
-#   #  print(nrow(InstanceData[[i]]))
-#   # print(nrow(FeatureData[[i]]))
-# }
-# 
-# 
+Diag <- diag(apply(ProxTrain, 1, sum))
+U<-Diag-ProxTrain
+
+k   <- length(behaviors)
+evL <- eigs_sym(U,k+1,which='SM')
+Z   <- t(evL$vectors[,1:k])
+
+#Z is our projection operator
+
+#plot(Z, col=as.factor(AllInstanceData$behavior[ix]), pch=20)
+
+
+#Now project our testing data into K space
+
+kData<-Z %*% ProxTest
+
+#Intermediate step:
+#compare the out-of-sample classification performance of LDA trained on {labels, z}
+#to the original RF trained on {labels, featuredata}, and as a function of k
+
+lda_comparison<-lda(x=t(Z),grouping = behaviorAsFactor[ix[-iq]])
+
+
+lda_prediction<-predict(object = lda_comparison,newdata=t(kData),dimen = k)
+
+reference<-factor(testing_RF_predicitions,levels = levels(lda_prediction$class))
+
+lda_RF_confusion_matrix<-confusionMatrix(data =lda_prediction$class,reference = reference)
+
+
+#4.run an HMM with gaussian emission probs for the projected points in the k-space
+
+####learn the HMM using the labelled and unlabelled data in the k-space
+
+
+AllInstanceData$behavior[-ix]<-NA
+
+labelledInstance<-behaviorAsFactor[ix[-iq]]
+
+hmmData<-list()
+hmmData$s<-as.numeric(labelledInstance)
+hmmData$x<-t(Z)
+hmmData$N<-length(hmmData$s)
+class(hmmData)<-"hsmm.data"
+
+
+
+
+
+states<-AllInstanceData$behavior[ix[-iq]]
+states<-states[!is.na(states)]
+
+
+#calculate empirial transition matrix
+statesLength<-length(states)
+Trans<-table(states[1:statesLength-1],states[2:statesLength])
+Trans <- Trans / rowSums(Trans)
+
+mu<-list()
+sigma<-list()
+for (j in 1:J){
+  mu[[j]]<-colMeans(Z[which(AllInstanceData$behavior==states[j]),])
+  sigma[[j]]<-cov(Z[which(AllInstanceData$behavior==states[j]),])
+}
+
+B <- list(mu=mu,sigma=sigma)
+model <- hmmspec(init=init, trans = Trans, parms.emis = B,dens.emis = dmvnorm.hsmm)
+
+save(model,rf, file = file.path(resultsDataDirectory,paste0(participant,'HMMandRFmodel.R')))
+
+##Now train model
+
+#output<-hmmfit(x = hmmData,start.val = model,mstep=mstep.mvnorm,lock.transition=FALSE,tol=1e-08,maxit=1000)
+
+#train <- simulate(model,  nsim=100, seed=1234, rand.emis=rmvnorm.hsmm)
+
+smoothed<-predict(object = model,newdata = kData,method = 'viterbi')
+
+newLabels<-factor(smoothed$s)
+
+labelCode<-levels(behaviorAsFactor[ix])
+
+
+levels(newLabels)<-labelCode
+#newLabels<-as.character(newLabels)
+
+#Calculate Confusion matrix
+
+HMM_confusion_matrix<-confusionMatrix(data =newLabels,reference =  behaviorAsFactor[ix[iq]])
+
+#output LDA and HMM confusion matrices
+
+LDAperformance[[i]]<-lda_RF_confusion_matrix
+HMMperformance[[i]]<-HMM_confusion_matrix
+
+#write predicitions
+
+write.csv(x=testing_RF_predicitions,file = file.path(RFoutput,paste0(participant,'RFpred.csv')))
+write.csv(x=newLabels,file = file.path(HMMoutput,paste0(participant,'HMMpred.csv')))
+write.csv(x=behaviorAsFactor[ix[iq]],file = file.path(HMMoutput,paste0(participant,'true.csv')))
+
+
+}
+
+save(LDAperformance, HMMperformance, file = file.path(resultsDataDirectory,"Results.RData"))
--- clusterFunctions.R
+++ clusterFunctions.R
@@ -5,8 +5,9 @@ cleanData <- function(jointFiles,labelDirectory,dataDirectory,drop=dropvals,outp
   
   
   participantID<-gsub(jointFiles[4],pattern = ".csv",replacement = '')
+  cat(paste0('starting data processing/loading for ', participantID,'\n'))
   
-  if(file.exists(file.path(instanceLabelDirectory,jointFiles[4]))==FALSE){
+  if(file.exists(file.path(instanceLabelDirectory,paste0(participantID,'Clean.csv')))==FALSE){
     
     labelData<-read.csv(file.path(labelDirectory,jointFiles[1]))
     keeps <- c('participant','label','startTime','endTime')
@@ -41,9 +42,10 @@ cleanData <- function(jointFiles,labelDirectory,dataDirectory,drop=dropvals,outp
       
     #Load up Feature Data
     featureData<-fread(file.path(dataDirectory,jointFiles[2]))
-    
+    featureData$identifier<-participantID
     #Load up FFT Data
     FFTData<-fread(file.path(dataDirectory,jointFiles[3]))
+    FFTData$identifier<-participantID
     
     
     
@@ -66,9 +68,12 @@ cleanData <- function(jointFiles,labelDirectory,dataDirectory,drop=dropvals,outp
     end_row<-end_row[iEnd]
     labelData$EndDateTime[nrows]<-end_times[iEnd]
     
-    #store data points outside labelled epoch
-    labelledFeatureData<-featureData[start_row:end_row,]
-    labelledFFTData<-FFTData[start_row:end_row,]
+    labelledData<-list()
+    
+    #only store data points inside labelled epoch
+    # minus one as extractLabelsSingleFile does not output final data point for labels
+    labelledData$labelledFeatureData<-featureData[start_row:(end_row-1),]
+    labelledData$labelledFFTData<-FFTData[start_row:(end_row-1),]
     
     #now round labels to align with spacing of features
     labelData$StartDateTime<-round5secs(labelData$StartDateTime,startTime = labelData$StartDateTime[1])
@@ -77,41 +82,43 @@ cleanData <- function(jointFiles,labelDirectory,dataDirectory,drop=dropvals,outp
     #now we need to turn from bout level labels to annotations. Here we use a slightly modified TLBC function, annotationsToLabels
     extractLabelsSingleFile(labelData, identifier= participantID,winSize = 5,instanceLabelDirectory = instanceLabelDirectory)
     
-    instanceLabelData<-fread(file.path(instanceLabelDirectory, paste0(participantID,'ALL.csv')))
+    labelledData$instanceLabelData<-fread(file.path(instanceLabelDirectory, paste0(participantID,'ALL.csv')))
+    labelledData$instanceLabelData$identifier<-participantID
     
-    print(paste0('Writing ALL feature data file ', participantID,'.csv'))
-    write.csv(x=labelledFeatureData,file=paste0(outputDataDirectory,'/', participantID,'ALLFeature.csv'),row.names=FALSE)
+    cat(paste0('Writing ALL feature data file ', participantID,'.csv','\n'))
+    write.csv(x=labelledData$labelledFeatureData,file=paste0(outputDataDirectory,'/', participantID,'ALLFeature.csv'),row.names=FALSE)
     
-    print(paste0('Writing ALL FFT data file ', participantID,'.csv'))
-    write.csv(x=labelledFFTData,file=paste0(outputDataDirectory,'/', participantID,'ALLFFT.csv'),row.names=FALSE)
+    cat(paste0('Writing ALL FFT data file ', participantID,'.csv','\n'))
+    write.csv(x=labelledData$labelledFFTData,file=paste0(outputDataDirectory,'/', participantID,'ALLFFT.csv'),row.names=FALSE)
 
+    #remove unknown labels at beginning and end of period - this is the 'clean' data
+    cleanData<-removeUnlabelledEnds(data=labelledData)
+   
+    cat(paste0('Writing CLEAN instance data file ', participantID,'.csv','\n'))
+    write.csv(x=cleanData$cleanInstanceLabelData,file=paste0(instanceLabelDirectory,'/', participantID,'Clean.csv'),row.names=FALSE)
     
+    cat(paste0('Writing CLEAN feature data file ', participantID,'.csv','\n'))
+    write.csv(x=cleanData$cleanLabelledFeatureData,file=paste0(outputDataDirectory,'/', participantID,'CleanFeature.csv'),row.names=FALSE)
     
-    #discard all feaure data before first labelled data point and after last labelled point
-    kx<-which(!instanceLabelData$behavior=='unknown')
-    if(length(kx)>0){
-      maxIndex<-min(kx[length(kx)],nrow(labelledFeatureData))
-      instanceLabelData<-instanceLabelData[kx[1]:maxIndex,]
-      
-      labelledFeatureData<-labelledFeatureData[kx[1]:maxIndex,]
-      labelledFFTData<-labelledFFTData[kx[1]:maxIndex,]
-    }
+    cat(paste0('Writing CLEANFFT data file ', participantID,'.csv','\n'))
+    write.csv(x=cleanData$cleanLabelledFFTData,file=paste0(outputDataDirectory,'/', participantID,'CleanFFT.csv'),row.names=FALSE)
     
-    print(paste0('Writing CLEAN instance data file ', participantID,'.csv'))
-    write.csv(x=instanceLabelData,file=paste0(instanceLabelDirectory,'/', participantID,'Clean.csv'),row.names=FALSE)
+      } else {
+    cat(paste0('processed files for ', participantID,' already exist. Loading...','\n'))
+        
+    labelledData<-list()
+    cleanData<-list()
     
-    print(paste0('Writing CLEAN feature data file ', participantID,'.csv'))
-    write.csv(x=labelledFeatureData,file=paste0(outputDataDirectory,'/', participantID,'CleanFeature.csv'),row.names=FALSE)
+    labelledData$instanceLabelData<-fread(file.path(instanceLabelDirectory, paste0(participantID,'ALL.csv')))
+    labelledData$labelledFeatureData<-fread(input =paste0(outputDataDirectory,'/', participantID,'ALLFeature.csv'))
+    labelledData$labelledFFTData<-fread(input=paste0(outputDataDirectory,'/', participantID,'ALLFFT.csv'))
+ 
+    cleanData$cleanInstanceLabelData<-fread(input = paste0(instanceLabelDirectory,'/', participantID,'Clean.csv'))
+    cleanData$cleanLabelledFeatureData<-fread(input=paste0(outputDataDirectory,'/', participantID,'CleanFeature.csv'))
+    cleanData$cleanLabelledFFTData<-fread(input=paste0(outputDataDirectory,'/', participantID,'CleanFFT.csv'))
     
-    print(paste0('Writing CLEANFFT data file ', participantID,'.csv'))
-    write.csv(x=labelledFFTData,file=paste0(outputDataDirectory,'/', participantID,'CleanFFT.csv'),row.names=FALSE)
-    
-      return(list(instanceLabelData,labelledFeatureData,labelledFFTData))
-
-  } else {
-    print(paste0('error: files for ', participantID,'.csv already exists'))
- 	return(list())
-  }
+      }
+  return(list(cleanData=cleanData,labelledData=labelledData))
   
 }
 
@@ -168,7 +175,7 @@ extractLabelsSingleFile = function(all_bouts,identifier, winSize,instanceLabelDi
     if (file.exists(out)) {
       file.remove(out)
     }
-    cat("timestamp,behavior\n", file=out, append=TRUE)
+    cat("identifier,timestamp,behavior\n", file=out, append=TRUE)
     
     while (TRUE) {
       if ((timestamp >= boutstart) & (timestamp + winSize <= boutstop)) {
@@ -201,6 +208,7 @@ extractLabelsSingleFile = function(all_bouts,identifier, winSize,instanceLabelDi
           }
         }
       }
+      cat(paste0(identifier,','), file=out, append=TRUE)
       cat(strftime(timestamp, "%Y-%m-%d %H:%M:%S,"), file=out, append=TRUE)
       cat(label, file=out, append=TRUE)
       cat("\n", file=out, append=TRUE)
@@ -212,3 +220,110 @@ extractLabelsSingleFile = function(all_bouts,identifier, winSize,instanceLabelDi
   }
   return(actNames)
 }
+
+
+removeUnlabelledEnds<-function(data,unlabelledBehaviorName='unknown'){
+
+kx<-which(!data$instanceLabelData$behavior==unlabelledBehaviorName)
+if(length(kx)>0){
+  maxIndex<-min(kx[length(kx)],nrow(data$labelledFeatureData))
+  
+  cleanInstanceLabelData<-data$instanceLabelData[kx[1]:maxIndex,]
+  cleanLabelledFeatureData<-data$labelledFeatureData[kx[1]:maxIndex,]
+  cleanLabelledFFTData<-data$labelledFFTData[kx[1]:maxIndex,]
+}
+return(list(cleanInstanceLabelData=cleanInstanceLabelData,cleanLabelledFeatureData=cleanLabelledFeatureData,cleanLabelledFFTData=cleanLabelledFFTData))
+}
+
+
+#Function to compute proximity between two node matricies -  
+computeProximity<-function(nodes1,nodes2,parallel=FALSE,mc.cores=1){
+  
+  #Check that we have the right number of columns
+  if(!ncol(nodes1)==ncol(nodes2)){stop( "number of columns do not match!" )}
+
+  ntrees<-ncol(nodes1)
+  
+  
+  P<-matrix(nrow = nrow(nodes1),ncol = nrow(nodes2),dimnames = NULL)
+  
+  if(parallel==FALSE){
+    P <- apply(
+      nodes1,
+      MARGIN = 1,
+      function(a){apply(
+        nodes2,
+        MARGIN = 1,
+        function(b){sum(a==b)})}) 
+    return(P/ntrees)
+    
+  }else if (parallel==TRUE){
+    coresPerLevel<-floor(sqrt(mc.cores))
+    
+    nrows<-nrow(nodes1)
+    
+    
+    #P=sapply(X = 
+               
+    P=mclapply(
+      X=split(nodes2,row(nodes2)),
+      FUN=function(a){apply(
+        nodes1,
+        MARGIN = 1,
+        function(b) sum(a==b))
+      })
+      #,FUN = cbind)
+    
+    return(matrix(unlist(P), ncol = length(P[[1]]), byrow = TRUE,dimnames = NULL)/ntrees)
+ 
+    
+  }
+}
+
+
+
+# 
+# 
+# P <- rbind(mclapply(
+#   nodes1[,],
+#   function(a){apply(
+#     nodes2,
+#     MARGIN = 1,
+#     function(b) sum(a==b))
+#   }
+#   ,mc.cores = mc.cores
+# ))
+# return(matrix(unlist(P), ncol = length(P[[1]]), byrow = TRUE))
+# 
+# outerFunct <- function(x, cores, funceval){
+#   require(parallel)
+#   mclapply(1:cores, funceval)
+# }
+# 
+# require(doMC)  
+# 
+# registerDoMC(mc.cores)
+# 
+# 
+# P <- foreach(b=nodes2, .combine='cbind') %:%
+#   foreach(a=nodes1, .combine='c') %dopar% {
+#     sum(a==b)
+#   }
+
+
+func1 <- 'NumericMatrix mmult1(NumericMatrix a, NumericMatrix b) {
+  int acoln = a.ncol();
+  int bcoln = b.ncol();
+  NumericMatrix out = no_init_matrix(a.nrow(), acoln + bcoln);
+  for (int j = 0; j < acoln + bcoln; j++) {
+    if (j < acoln) {
+      out(_, j) = a(_, j);
+    } else {
+      out(_, j) = b(_, j - acoln);
+    }
+  }
+  return out;
+}'
+
+cppFunction(func1)
+
