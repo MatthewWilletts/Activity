6bff14ec9a3c11870fb31192fb2a7572a1771cf8
Matthew Willetts <matthewwilletts@users.noreply.github.com>
2016-06-27 19:23:11 +0100

    sparse script

---
+++ Rscripts/7_clusterAnalysis_ProxTrain_sparse.R
@@ -0,0 +1,53 @@
+#MultiCore analysis on ARCUS B cluster of 120 or so participants labelled data, using the Holmes Algorithm
+
+#load packages
+source('/home/dph-ukbaccworkgroup/magd4534/Activity/clusterPackages.R')
+
+
+
+#In this script we will be doing 'leave one out' analysis for our participants
+
+
+source('/home/dph-ukbaccworkgroup/magd4534/Activity/clusterFunctions.R')
+
+
+#Take in arguements from command line
+#This script will calculate a chunk of the ProxTrain matrix
+
+#So we need to know:
+#a) how many chunks job is divided into
+#b) which chunk we are doing
+#c) which participant we are leaving out
+
+#parse inputs
+source('/home/dph-ukbaccworkgroup/magd4534/Activity/clusterInputs.R')
+
+
+ncores<-16
+
+set.seed(chunkID)
+
+registerDoMC(ncores/2)
+
+#define data directories
+source('/home/dph-ukbaccworkgroup/magd4534/Activity/clusterDirectories.R')
+
+
+#load participants
+load(file =file.path(resultsDataDirectory,paste0('participants_',duration,'.RData')))
+
+#load mean values
+
+ProxTrain_dt<-fread(input =file.path(ProxOutput,paste0('ProxTrain_',participants[leave_out],'.csv')),skip=1,header = FALSE)
+ProxTrain_matrix<-as.matrix(ProxTrain_dt)
+rm(ProxTrain_dt)
+
+#Convert to sparse
+ProxTrain_sparse<-as(ProxTrain_matrix,'dsRMatrix')
+
+cat('conversion complete \n')
+
+cat('saving \n')
+
+writeMM(ProxTrain_sparse,file.path(ProxOutput,'ProxTrain_sparse'))
+
